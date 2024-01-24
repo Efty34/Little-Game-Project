dcec6347494041b1caf1cf87864bd1018584ba34
Efty34 <efty17.hasan@gmail.com>
2024-01-24 22:30:33 +0600

    Crucial stage

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
 const KeyboardDiv = document.querySelector(".keyboard");
+
+let currentWord, wrongGuessCount = 0;
+const maxGuesses = 6;
 
 const getRandomWord = () => {
     const{ word, hint }  = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word, hint);
+    currentWord = word;
+    console.log(word);
     document.querySelector(".hint-text b").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(( )=> '<li class="letter"></li>').join("");
+    wordDisplay.innerHTML = word.split("").map(( )=> `<li class="letter"></li>`).join("");
 }
 
 const initGame = (button, clickedLetter) => {
-    console.log(button, clickedLetter);
+    if(currentWord.includes(clickedLetter)){
+        [...currentWord].forEach((letter, index)=>{
+            if(letter===clickedLetter){
+                wordDisplay.querySelectorAll("li")[index].innerText=letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+    }
+    else{
+        wrongGuessCount++;
+    }
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 }
 
 for (let i = 97; i <= 122; i++) {
